ac02026a5c8b0d8a796105fb9c151a5f6b3049eb
seanp <seanpatmeade@gmail.com>
2019-04-28 15:01:25 +0100

    Added comments to first half of code.

--- fisherIrisData.py
+++ fisherIrisData.py
@@ -2,13 +2,16 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Taking in data from the csv file
 with open('iris.csv', 'r') as csvFile:
+    # Reading that data
     reader = csv.reader(csvFile)
-    x = 0
+    # Create lists for seperated data
     setosaList = []
     versicolorList = []
     virginicaList = []
     
+    # Seperate data in accordance with the iris type
     for row in reader:
         if row[4] == "Iris-setosa":
             setosaList.append(row[0:4])
@@ -22,16 +25,19 @@ with open('iris.csv', 'r') as csvFile:
         else:
             print("Unlabled entry.")
         
-
+# Check to see if all data is there (50 in each)
 if len(virginicaList) != 50 | len(versicolorList) != 50 | len(setosaList):
         print("Error with size of Lists.")
 
+# Convert lists to numpy arrays
 Setosa = np.array(setosaList).astype(np.float)
 Versicolor = np.array(versicolorList).astype(np.float)
 Virginica = np.array(virginicaList).astype(np.float)
 
+# Make an array with each iris as an element for easier access
 flowers = [Setosa, Versicolor, Virginica]
 
+# Function that will go through and lable iris type (name) and call minMaxMean
 def allFlowers(allFlowers):
     for i in range(0,len(allFlowers)):
         
@@ -45,19 +51,23 @@ def allFlowers(allFlowers):
             print("Error with data set. Please investigate.")
         minMaxMin(allFlowers[i], name)
 
-
+# Calculate min, max and mean of each element of the data for each iris
 def minMaxMin(arrayName, name):
 
     name = name
+
     SepLmin = np.min(arrayName[:,0])
     SepLmax = np.max(arrayName[:,0])
     SepLmean = np.mean(arrayName[:,0])
+    
     SepWmin = np.min(arrayName[:,1])
     SepWmax = np.max(arrayName[:,1])
     SepWmean = np.mean(arrayName[:,1])
+    
     PetLmin = np.min(arrayName[:,2])
     PetLmax = np.max(arrayName[:,2])
     PetLmean = np.mean(arrayName[:,2])
+    
     PetWmin = np.min(arrayName[:,3])
     PetWmax = np.max(arrayName[:,3])
     PetWmean = np.mean(arrayName[:,3])
@@ -67,5 +77,7 @@ def minMaxMin(arrayName, name):
         f"Petal length min, max and mean: {PetLmin}, {PetLmax}, {PetLmean}\n"
         f"Petal width min, max and mean: {PetWmin}, {PetWmax}, {PetWmean}")
 
-
+# Call function to go through each Iris and produce min, max and mean
 allFlowers(flowers)
+
+# Creating scatter plots
