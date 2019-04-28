ff5401a928a4f58f0dd887c86c9328266bdcce17
seanp <seanpatmeade@gmail.com>
2019-04-28 22:19:04 +0100

    Trying to figure out how to compare arrays to determine the new iris.

--- README.md
+++ README.md
@@ -16,12 +16,46 @@ Ronald Fisher used measurements of the sepal and petal of three different types 
 
 and length of both the sepal and petal (fifty times for each). Figure 1 shows how similar the three 
 
-irises are and also indicates where the petal and sepal are on the iris plant.
+irises are and indicates where the petal and sepal are on the iris plant.
 
 
 
-![Image of All three Iris flowers](Images-and-tables/IrisComparison.png)
+![All three Iris flowers](Images-and-tables/IrisComparison.png)
 
 
 
 ## So who was Ronald Fisher?
+
+Sir Ronald Aylmer Fisher (to give him his full title) was a British Statistician and Geneticist. Born in 
+East Finchley in 1890 Fisher started to show a “special ability” when it came to mathematics at an 
+early age. He had also from an early age gained a strong biological interest. As a result, he was 
+unsure whether to go into mathematics or biology. Choosing mathematics at Cambridge he came 
+across a book by Karl Pearson titled Mathematical contributions to the theory of evolution. After this 
+he was interested in problems to do with evolution and genetics.
+
+After University he took up work in an investment company and then moved on to working on a 
+farm in Canada, no doubt bored with what would have been simple work for a mathematician of his 
+ability. He also tried to join the army in 1914 but was rejected due to his terrible eye sight. For the 
+next five years he would teach as a mathematics and physics at various public schools. 
+
+He never lost his interest in mathematics and genetics though and through this time he was 
+developing his own ideas. These ideas brought on interest and job offers. From here he had more 
+freedom and resources to pursue his work eventually leading him to produce The use of multiple 
+measurements in taxonomic problems amongst many others. Fisher despite his natural and 
+developed abilities clearly pursued knowledge over financial gain which is what led him to be such 
+an influential person in statistics and is considered “the single most important figure in 20th century 
+statistics”.[1]
+
+## An in-depth Look at the Data Set:
+
+First off, I think it would help to have a visual representation of the data set. Below is a table 
+containing the first 4 rows of the data set and the headings of each column.
+
+![First four rows of the data set](Images-and-tables/firstFourRows.png)
+
+The heading of each column (i.e. sepal length, sepal width, petal length etc.) are called attributes. 
+You can think of attributes as features each flower has. 
+
+This data set contains 150 rows. This is further split into three. The first 50 are the measurements 
+from setosa. The second 50 are from versicolor. The third set of 50 are from virginica.
+
--- fisherIrisData.py
+++ fisherIrisData.py
@@ -64,23 +64,28 @@ def minMaxMin(arrayName, name):
     SepLmin = np.min(arrayName[:,0])
     SepLmax = np.max(arrayName[:,0])
     SepLmean = np.mean(arrayName[:,0])
+    SepLstd = np.std(arrayName[:,0])
     
     SepWmin = np.min(arrayName[:,1])
     SepWmax = np.max(arrayName[:,1])
     SepWmean = np.mean(arrayName[:,1])
+    SepWstd = np.std(arrayName[:,1])
     
     PetLmin = np.min(arrayName[:,2])
     PetLmax = np.max(arrayName[:,2])
     PetLmean = np.mean(arrayName[:,2])
+    PetLstd = np.std(arrayName[:,2])
     
     PetWmin = np.min(arrayName[:,3])
     PetWmax = np.max(arrayName[:,3])
     PetWmean = np.mean(arrayName[:,3])
+    PetWstd = np.std(arrayName[:,3])
 
-    print(f"\nFor {name}:\nSepal Length min, max and mean:{SepLmin}, {SepLmax}, {SepLmean}\n"
-        f"Sepal width min, max and mean: {SepWmin}, {SepWmax}, {SepWmean}\n"
-        f"Petal length min, max and mean: {PetLmin}, {PetLmax}, {PetLmean}\n"
-        f"Petal width min, max and mean: {PetWmin}, {PetWmax}, {PetWmean}")
+    print(f"\nFor {name}:\n"
+            f"Sepal Length min, max, mean and standard deviation:{round(SepLmin, 2)}, {round(SepLmax, 2)}, {round(SepLmean, 2)}, {round(SepLstd, 2)}\n"
+            f"Sepal width min, max, mean and standard deviation: {round(SepWmin, 2)}, {round(SepWmax, 2)}, {round(SepWmean, 2)}, {round(SepWstd, 2)}\n"
+            f"Petal length min, max, mean and standard deviation: {round(PetLmin, 2)}, {round(PetLmax, 2)}, {round(PetLmean, 2)}, {round(PetLstd, 2)}\n"
+            f"Petal width min, max, mean and standard deviation: {round(PetWmin, 2)}, {round(PetWmax, 2)}, {round(PetWmean, 2)}, {round(PetWstd, 2)}")
     print("\n")
 
 # Call function to go through each Iris and produce min, max and mean
@@ -110,9 +115,12 @@ for data, color, group in zip(data, colors, groups):
     x, y = data
     ax.scatter(x, y, alpha = 0.8, c=color, edgecolors='none', s=30, label=group)
 
+plt.xlabel("length (cm)")
+plt.ylabel("width (cm)")
+
 plt.title("Sepal Length vs Sepal Width")
 plt.legend(loc=2)
-plt.show()
+'''plt.show()'''
 
 # Second scatter plot: Petal length vs. Petal width
 
@@ -136,9 +144,12 @@ for data, color, group in zip(data, colors, groups):
     x, y = data
     ax.scatter(x, y, alpha = 0.8, c=color, edgecolors='none', s=30, label=group)
 
+plt.xlabel("length (cm)")
+plt.ylabel("width (cm)")
+
 plt.title("Petal Length vs Petal Width")
 plt.legend(loc=2)
-plt.show()
+'''plt.show()'''
 
 # Third scatter plot: Sepal length*width vs. Petal length*width
 
@@ -177,15 +188,17 @@ for data, color, group in zip(data, colors, groups):
     x, y = data
     ax.scatter(x, y, alpha = 0.8, c=color, edgecolors='none', s=30, label=group)
 
+plt.xlabel("Sepal L x W (cm^2)")
+plt.ylabel("Petal L X W (cm^2)")
+
 plt.title("Sepal Length by Width vs. Petal Length by Width")
 plt.legend(loc=2)
-plt.show()
+'''plt.show()'''
 
 # K-Nearest Neighbors
 # Using this algorithm wit the data we have we can find a new data entry (an unidentified iris)
 # and find a closest match to it.
 
-from sklearn import datasets
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import StandardScaler
 
@@ -212,6 +225,18 @@ distances, indices = nearest_neighbors.kneighbors([new_entry])
 
 nearestN = data_standardized[indices]
 
-print(f"The two nearest fits are {nearestN[0][0]} and {nearestN[0][1]}.)
+nearestN = np.array(nearestN).astype(np.float)
+# [[nearestN[0][0][0], nearestN[0][0][1], nearestN[0][0][2], nearestN[0][0][3]], [nearestN[0][1][0], nearestN[0][1][1], nearestN[0][1][2], nearestN[0][1][3]]]
+print(f"The two nearest fits are {nearestN[0][0]} and {nearestN[0][1]}")
+print(nearestN[0])
+print(Setosa[1])
+for i in range(0, len(setosaList)):
+    if Setosa[i] == nearestN[0] | Setosa[i] == nearestN[1]:
+        print("It matches close to a Setosa in data set.")
+    elif Virginica[i] == nearestN[0] | Virginica[i] == nearestN[1]:
+        print("It matches close to a virginica in data set.")
+    elif Versicolor[i] == nearestN[0] | Versicolor[i] == nearestN[1]:
+        print("It matches close to a versicolor in data set.")
+
 
 # By these outputs it can be determined to some degree of sucess which iris it is.
